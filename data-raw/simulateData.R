5db0c23cfd8ed62803399306890a5676d4bfb424
Selk19 <svenja.elkenkamp@uni-bielefeld.de>
2025-09-18 17:13:59 +0200

    named stv as beta

--- data-raw/simulateData.R
+++ data-raw/simulateData.R
@@ -3,8 +3,7 @@
 
 
 
-# these datasets can be used in Tests file to test the functions
-
+# these datasets can be used in the examples and the Tests file to test the functions
 
 ######################################
 ### Random Numbers from NormalDist ###
@@ -17,16 +16,17 @@ n_samples_tto <- 200
 n_samples_dce <- 100
 sigma <- 1.0
 theta <- 5
-stv <- c(0.5, -0.3, 0.8)  # coef for x
+beta <- c(0.5, -0.3, 0.8)  # true coef for beta
 
 # simulate matrix
-x_tto <- matrix(rnorm(n_samples_tto * length(stv)), ncol = length(stv))
-x_dce <- matrix(rnorm(n_samples_dce * length(stv)), ncol = length(stv))
+x_tto <- matrix(rnorm(n_samples_tto * length(beta)), ncol = length(beta))
+x_dce <- matrix(rnorm(n_samples_dce * length(beta)), ncol = length(beta))
 
-# Xb (linear predictors) and y
-Xb_tto <- x_tto %*% stv
-Xb_dce <- (x_dce %*% stv) * theta
+# Xb (linear predictor following formula xb = x1*beta1 + x2*beta2 + x3*beta3)
+Xb_tto <- x_tto %*% beta
+Xb_dce <- (x_dce %*% beta) * theta
 
+# generate y
 y_tto <- rnorm(n_samples_tto, mean = Xb_tto, sd = sigma)  # continuous outcomes
 logistic_tmp <- 0.5 + 0.5 * tanh(Xb_dce / 2)
 y_dce <- rbinom(n_samples_dce, size = 1, prob = logistic_tmp)  # binary outcomes
@@ -61,17 +61,18 @@ n_samples_tto <- 200
 n_samples_dce <- 100
 sigma <- 0.5
 theta <- 2
-stv <- c(1.4, 2.3, -0.2)  # coef for x
+beta <- c(1.4, 2.3, -0.2)  # true coef for beta
 
 
 # simulate matrix
-x_tto <- matrix(rnorm(n_samples_tto * length(stv)), ncol = length(stv))
-x_dce <- matrix(rnorm(n_samples_dce * length(stv)), ncol = length(stv))
+x_tto <- matrix(rnorm(n_samples_tto * length(beta)), ncol = length(beta))
+x_dce <- matrix(rnorm(n_samples_dce * length(beta)), ncol = length(beta))
 
-# Xb (linear predictors) and y
-Xb_tto <- x_tto %*% stv
-Xb_dce <- (x_dce %*% stv) * theta
+#  Xb (linear predictor following formula xb = x1*beta1 + x2*beta2 + x3*beta3)
+Xb_tto <- x_tto %*% beta
+Xb_dce <- (x_dce %*% beta) * theta
 
+# generate y
 y_tto <- rnorm(n_samples_tto, mean = Xb_tto, sd = sigma)  # continuous outcomes
 logistic_tmp <- 0.5 + 0.5 * tanh(Xb_dce / 2)
 y_dce <- rbinom(n_samples_dce, size = 1, prob = logistic_tmp)  # binary outcomes
@@ -108,18 +109,12 @@ simulated_data1$id <- c(1:100, 1:100, 1:100)
 simulated_data2$id <- c(101:200, 101:200, 101:200)
 
 
-simulated_data <- rbind(simulated_data1,simulated_data2)
-simulated_data_too <- simulated_data[simulated_data$type == "TTO",]
-simulated_data_dce <- simulated_data[simulated_data$type == "DCE_A",]
+simulated_data_norm<- rbind(simulated_data1,simulated_data2)
 
 
-simulated_data_norm <- simulated_data
-
-rm(simulated_data)
 rm(simulated_data1)
 rm(simulated_data2)
-rm(simulated_data_dce)
-rm(simulated_data_too)
+
 
 
 
@@ -137,7 +132,7 @@ n_samples_tto <- 120
 n_samples_dce <- 120
 sigma <- 0.001
 theta <- 0.2
-stv <- c(0.005, 0.01, 0.08, 0.1) # MO
+beta <- c(0.005, 0.01, 0.08, 0.1) # true coef for beta mo2,mo3,mo4,mo5
 
 
 
@@ -149,9 +144,9 @@ dummy_mo <- dummy_mo[,-1]
 
 
 
-# linear predictor
-Xb_tto <- as.matrix(dummy_mo) %*% stv
-Xb_dce <- (as.matrix(dummy_mo) %*% stv) * theta
+# linear predictor xb = mo2*beta2 + mo3*beta3 + mo4*beta4 + mo5*beta5
+Xb_tto <- as.matrix(dummy_mo) %*% beta
+Xb_dce <- (as.matrix(dummy_mo) %*% beta) * theta
 
 # y
 y_tto <- rnorm(n_samples_tto, mean = Xb_tto, sd = sigma)
@@ -205,7 +200,7 @@ n_samples_tto <- 120
 n_samples_dce <- 120
 sigma <- 0.1
 theta <- 2
-stv <- c(0.2, 0.4, 0.6, 0.8) # MO
+beta <- c(0.2, 0.4, 0.6, 0.8) # true coef for beta mo2,mo3,mo4,mo5
 
 
 
@@ -217,9 +212,9 @@ dummy_mo <- dummy_mo[,-1]
 
 
 
-# linear predictor
-Xb_tto <- as.matrix(dummy_mo) %*% stv
-Xb_dce <- (as.matrix(dummy_mo) %*% stv) * theta
+# linear predictor xb = mo2*beta2 + mo3*beta3 + mo4*beta4 + mo5*beta5
+Xb_tto <- as.matrix(dummy_mo) %*% beta
+Xb_dce <- (as.matrix(dummy_mo) %*% beta) * theta
 
 # y
 y_tto <- rnorm(n_samples_tto, mean = Xb_tto, sd = sigma)
@@ -271,7 +266,7 @@ n_samples_tto <- 120
 n_samples_dce <- 120
 sigma <- 0.1
 theta <- 3
-stv <- c(0.001, 0.05, 0.08, 0.1, # MO
+beta <- c(0.001, 0.05, 0.08, 0.1, # MO
          0.01, 0.2, 0.36, 0.5, # SC
          0.015, 0.25, 0.5, 0.8, # UA
          0.1, 0.3, 0.4, 0.6, # PD
@@ -306,7 +301,7 @@ dummy_ad <- dummy_ad[,-1]
 
 
 
-# alle zusammen
+# all together
 dummy_df <- cbind(dummy_mo, dummy_sc)
 dummy_df <- cbind(dummy_df, dummy_ua)
 dummy_df <- cbind(dummy_df, dummy_pd)
@@ -315,8 +310,8 @@ dummy_df <- cbind(dummy_df, dummy_ad)
 
 
 # linear predictor
-Xb_tto <- as.matrix(dummy_df) %*% stv
-Xb_dce <- (as.matrix(dummy_df) %*% stv) * theta
+Xb_tto <- as.matrix(dummy_df) %*% beta
+Xb_dce <- (as.matrix(dummy_df) %*% beta) * theta
 
 # y
 y_tto <- rnorm(n_samples_tto, mean = Xb_tto, sd = sigma)
@@ -380,25 +375,6 @@ simulated_data1$id <- c(1:n_samples_tto,1:n_samples_dce)
 
 
 
-# check with base R functions for each type
-lm(y~ -1 + mo2 + mo3 + mo4 + mo5 +
-     sc2 + sc3 + sc4 + sc5 +
-     ua2 + ua3 + ua4 + ua5 +
-     pd2 + pd3 + pd4 + pd5 +
-     ad2 + ad3 + ad4 + ad5,
-   # data = data_tto,
-   data = simulated_data1[simulated_data1$type == "TTO",])
-
-
-
-glm(y~ -1 + mo2 + mo3 + mo4 + mo5 +
-      sc2 + sc3 + sc4 + sc5 +
-      ua2 + ua3 + ua4 + ua5,
-    data = simulated_data1[simulated_data1$type == "DCE_A",], family = "binomial")
-
-
-
-
 
 ### COMP 2 ###
 
@@ -408,7 +384,7 @@ n_samples_tto <- 120
 n_samples_dce <- 120
 sigma <- 0.02
 theta <- 2
-stv <- c(0.2, 0.4, 0.6, 0.8, # MO
+beta <- c(0.2, 0.4, 0.6, 0.8, # MO
          0.1, 0.3, 0.4, 0.5, # SC
          0.2, 0.25, 0.6, 0.7, # UA
          0.05, 0.2, 0.27, 0.8, # PD
@@ -451,8 +427,8 @@ dummy_df <- cbind(dummy_df, dummy_ad)
 
 
 # linear predictor
-Xb_tto <- as.matrix(dummy_df) %*% stv
-Xb_dce <- (as.matrix(dummy_df) %*% stv) * theta
+Xb_tto <- as.matrix(dummy_df) %*% beta
+Xb_dce <- (as.matrix(dummy_df) %*% beta) * theta
 
 # y
 y_tto <- rnorm(n_samples_tto, mean = Xb_tto, sd = sigma)
@@ -526,9 +502,6 @@ simulated_data <- rbind(simulated_data1, simulated_data2)
 
 
 
-
-
-
 ##########################################
 ############ CENSORED DATA ###############
 ##########################################
@@ -537,7 +510,7 @@ simulated_data <- rbind(simulated_data1, simulated_data2)
 ### Random Numbers from NormalDist ###
 ######################################
 
-# upper bound for data1$y at 3
+# upper bound for simulated_data_norm$y at 3
 # new variable y_cens
 
 for(i in 1:length(simulated_data_norm$y)){
@@ -555,7 +528,7 @@ for(i in 1:length(simulated_data_norm$y)){
 ### ONLY MOBILITY ###
 #####################
 
-# lower bound for simualted_data$y at 0
+# lower bound for simualted_data_mo$y at 0
 # new variable y_cens
 
 for(i in 1:length(simulated_data_mo$y)){
